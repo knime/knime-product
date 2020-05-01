d5ddb0805c98297d5f3afc41846d049ceda689cb
Bernd Wiswedel <Bernd.Wiswedel@knime.com>
2020-05-01 10:34:39 +0200

    AP-13377: removed Windows 32-bit support
    AP-13377: Update KNIME Analytics Platform to use the latest Eclipse

--- org.knime.update.product/fix-windows-launcher.sh
+++ org.knime.update.product/fix-windows-launcher.sh
@@ -1,3 +1,2 @@
 cd target/repository/binary
-zip -9j org.knime.product.desktop.executable.win32.win32.x86_[0-9].* "../../../win32/x86/knime.exe"
 zip -9j org.knime.product.desktop.executable.win32.win32.x86_64_* "../../../win32/x86_64/knime.exe"

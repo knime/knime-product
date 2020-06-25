2c5239babfc99f7156d836fa75566e8d696e14d6
Thorsten Meinl <thorsten.meinl@knime.com>
2020-06-25 13:39:59 +0200

    Fixed replacing of knime.exe with our modified version

--- org.knime.update.product/fix-windows-launcher.sh
+++ org.knime.update.product/fix-windows-launcher.sh
@@ -1,2 +1,2 @@
 cd target/repository/binary
-zip -9j org.knime.product.desktop.executable.win32.win32.x86_64_* "../../../win32/x86_64/knime.exe"
+zip -9j org.knime.desktop.product.executable.win32.win32.x86_64_* "../../../win32/x86_64/knime.exe"

b52315ab1cf9654504c630240e8a60d6f7d19f3e
Martin Horn <martin.horn@knime.com>
2023-03-03 16:14:56 +0100

    Only set 'chromium.args' system property if not already set
    Allows one to start the AP with a different set of 'chromium.args'
    defined via the knime.ini.

--- org.knime.product/src/org/knime/product/rcp/KNIMEApplication.java
+++ org.knime.product/src/org/knime/product/rcp/KNIMEApplication.java
@@ -171,7 +171,9 @@ public class KNIMEApplication implements IApplication {
             // Required to make the CEF browser work properly (in particular the so-called 'browser functions' which are
             // used to call java-functions from js).
             // With every CEF update we'll need to check whether this is still necessary.
-            System.setProperty("chromium.args", "--disable-web-security");
+            if (System.getProperty("chromium.args") == null) {
+                System.setProperty("chromium.args", "--disable-web-security");
+            }
 
             final var iniChangedChecker = new IniChangedChecker(!EclipseUtil.isRunFromSDK() ? getIniPath() : null)
                     .digestIni();

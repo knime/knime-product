677d3ab7c4414dd0b846376b1aa564074c0831c6
Gabriel Einsdorf <gabriel.einsdorf@knime.com>
2023-06-15 12:08:37 +0200

    DEVOPS-1794: Also update sha512sum in artifacts.xml
    DEVOPS-1794 (Windows workflow tests fails with SHA-512 hash installation errors )

--- org.knime.update.product/fix-windows-launcher.sh
+++ org.knime.update.product/fix-windows-launcher.sh
@@ -5,6 +5,7 @@ unzip artifacts.jar
 SIZE=$(stat -c %s binary/org.knime.desktop.product.executable.win32.win32.x86_64_*)
 MD5=$(md5sum binary/org.knime.desktop.product.executable.win32.win32.x86_64_* | awk '{ print $1 }')
 SHA=$(sha256sum binary/org.knime.desktop.product.executable.win32.win32.x86_64_* | awk '{ print $1 }')
+SHA_512=$(sha512sum binary/org.knime.desktop.product.executable.win32.win32.x86_64_* | awk '{ print $1 }')
 
 xmlstarlet ed -L \
     -u "/repository/artifacts/artifact[@id='org.knime.desktop.product.executable.win32.win32.x86_64']/properties/property[@name='download.size']/@value" -v $SIZE \
@@ -12,6 +13,7 @@ xmlstarlet ed -L \
     -u "/repository/artifacts/artifact[@id='org.knime.desktop.product.executable.win32.win32.x86_64']/properties/property[@name='download.md5']/@value" -v $MD5 \
     -u "/repository/artifacts/artifact[@id='org.knime.desktop.product.executable.win32.win32.x86_64']/properties/property[@name='download.checksum.md5']/@value" -v $MD5 \
     -u "/repository/artifacts/artifact[@id='org.knime.desktop.product.executable.win32.win32.x86_64']/properties/property[@name='download.checksum.sha-256']/@value" -v $SHA \
+    -u "/repository/artifacts/artifact[@id='org.knime.desktop.product.executable.win32.win32.x86_64']/properties/property[@name='download.checksum.sha-512']/@value" -v $SHA_512 \
     artifacts.xml
 
 zip -9 artifacts.jar artifacts.xml

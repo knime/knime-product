eb66d99f4b4a69955785f97b391c526468b659dc
Benjamin Moser <benjamin.moser@knime.com>
2024-12-20 09:43:11 +0100

    NXT-3047: Code style / sonar (no functional changes)
    (NXT-3047 new source for home page tile data)

--- org.knime.product/src/org/knime/product/rcp/intro/WelcomeAPEndpoint.java
+++ org.knime.product/src/org/knime/product/rcp/intro/WelcomeAPEndpoint.java
@@ -1,3 +1,49 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME AG, Zurich, Switzerland
+ *  Website: http://www.knime.org; Email: contact@knime.org
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME AG herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ */
 package org.knime.product.rcp.intro;
 
 import java.io.IOException;
@@ -75,6 +121,7 @@ public final class WelcomeAPEndpoint {
      * @param companyName Customisation information from AP instance, nullable. Already URL-encoded.
      * @return the home/welcome page content categories or an empty optional if run from the SDK
      */
+    @SuppressWarnings({"java:S2142", "java:S1166"})
     public Optional<JSONCategory[]> getCategories(final boolean calledFromWebUI, final String companyName) {
         if (m_future == null) {
             m_future = CompletableFuture.supplyAsync(() -> WelcomeAPEndpoint.request(calledFromWebUI, companyName));
@@ -93,7 +140,7 @@ public final class WelcomeAPEndpoint {
      * @param companyName Customisation information from AP instance, nullable. Already URL-encoded.
      * @return The home page tile contents, grouped into categories.
      */
-    private static Optional<JSONCategory[]> request(final boolean calledFromWebUI, final String companyName) { // NOSONAR
+    private static Optional<JSONCategory[]> request(final boolean calledFromWebUI, final String companyName) {
         if (EclipseUtil.isRunFromSDK()) {
             return Optional.empty();
         }
@@ -141,9 +188,9 @@ public final class WelcomeAPEndpoint {
             }
         }
         if (uiCustomizationOptional //
-                .map(UICustomization::getWelcomeAPEndpointURLType) //
-                .stream() //
-                .anyMatch(type -> type != WelcomeAPEndPointURLType.NONE)) {
+            .map(UICustomization::getWelcomeAPEndpointURLType) //
+            .stream() //
+            .anyMatch(type -> type != WelcomeAPEndPointURLType.NONE)) {
             return endpointToCategoriesMaps.values().stream().findFirst().map(l -> l.toArray(JSONCategory[]::new));
         }
         return Optional.empty();
@@ -151,8 +198,8 @@ public final class WelcomeAPEndpoint {
 
     /**
      * For user-defined endpoints, replace the placeholder "{user}" with the actual user name. In most cases (99.9%+)
-     * this method does nothing as the default endpoint in the public KNIME distribution is used
-     * (see {@link #ENDPOINT}). Custom Business-Hubs might deliver AP customizations with custom endpoints having place
+     * this method does nothing as the default endpoint in the public KNIME distribution is used (see
+     * {@link #ENDPOINT}). Custom Business-Hubs might deliver AP customizations with custom endpoints having place
      * holders for the user name. The user name is determined by {@link User#getUsername()}.
      *
      * @return The modified endpoint URL in case it contains the placeholder "{user}". Otherwise the input is returned.
@@ -229,7 +276,8 @@ public final class WelcomeAPEndpoint {
 
             final Supplier<Optional<ZonedDateTime>> m_lastSentUpload;
 
-            Scope(final String parameterName, final Supplier<Optional<ZonedDateTime>> lastLogin, final Supplier<Optional<ZonedDateTime>> lastUpload,
+            Scope(final String parameterName, final Supplier<Optional<ZonedDateTime>> lastLogin,
+                final Supplier<Optional<ZonedDateTime>> lastUpload,
                 final Supplier<Optional<ZonedDateTime>> lastSentLogin,
                 final Supplier<Optional<ZonedDateTime>> lastSentUpload) {
                 m_parameterName = parameterName;
@@ -240,10 +288,10 @@ public final class WelcomeAPEndpoint {
             }
         }
 
-        final String id;
+        final String m_id;
 
         HubUsage(final String identifier) {
-            id = identifier;
+            m_id = identifier;
         }
 
         /** Signal that hub usage for the given scope have now been reported to instrumentation */
@@ -286,7 +334,7 @@ public final class WelcomeAPEndpoint {
         }
 
         static String requestParameter(final Scope scope) {
-            return scope.m_parameterName + ":" + HubUsage.current(scope).id;
+            return scope.m_parameterName + ":" + HubUsage.current(scope).m_id;
         }
     }
 
